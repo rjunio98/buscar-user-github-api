7b5c3725a578e96406cc4fe89b60f66394044e4e
Robledo Junior <robledorjunior1@gmail.com>
2023-06-12 13:47:14 -0300

    cor img

--- src/js/services/repositories.js
+++
@@ -1,9 +0,0 @@
-import { baseUrl, repositoriesQuantity } from "src/js/variables.js"
-
-async function repositories(userName){
-    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
-    return await response.json()
-}
-
-export { repositories }
-
--- src/js/services/user.js
+++
@@ -1,8 +0,0 @@
-import { baseUrl } from "src/js/variables.js"
-
-async function user(userName){
-    const response = await fetch(`${baseUrl}/${userName}`)
-    return await response.json()
-}
-
-export { user }
